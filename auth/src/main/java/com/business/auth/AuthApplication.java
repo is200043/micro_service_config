fd0b0ba1fbdc14e32b0bf3aeb0f12279f8aeb956
Chonlakorn Punphopthaworn <chonlakorn.pun@gmail.com>
2018-11-08 12:42:46 +0700

    commit

--- auth/src/main/java/com/business/auth/AuthApplication.java
+++ auth/src/main/java/com/business/auth/AuthApplication.java
@@ -5,9 +5,11 @@ import com.business.auth.user.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AuthApplication {
 
     public static void main(String[] args) {
