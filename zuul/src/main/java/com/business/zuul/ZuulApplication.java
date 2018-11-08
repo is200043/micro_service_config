ec05f680c97d898f62f5f3544c70ad8cf2326022
Chonlakorn Punphopthaworn <chonlakorn.pun@gmail.com>
2018-11-08 14:33:53 +0700

    commit

--- zuul/src/main/java/com/business/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/business/zuul/ZuulApplication.java
@@ -1,13 +1,16 @@
 package com.business.zuul;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
+@AutoConfigureBefore(EurekaClientAutoConfiguration.class)
 public class ZuulApplication {
 
     public static void main(String[] args) {
