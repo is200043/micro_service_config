4953981f0d2db885cbb22b9e0c47164cc5ff2262
Chonlakorn Punphopthaworn <chonlakorn.pun@gmail.com>
2018-11-09 13:21:06 +0700

    commit

--- promotion/src/main/java/com/business/promotion/PromotionController.java
+++ promotion/src/main/java/com/business/promotion/PromotionController.java
@@ -7,6 +7,9 @@ package com.business.promotion;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,11 +24,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/promotion")
 public class PromotionController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(PromotionController.class);
+
     @Autowired
     PromotionRespository promotionRespository;
 
     @GetMapping("/{promotionCode}")
     public Map<String, Object> getPromotion(@PathVariable String promotionCode) {
+        LOGGER.info("start getPromotion");
         Map<String, Object> result = new HashMap<>();
         Promotion promotion = promotionRespository.findByCode(promotionCode);
         if (promotion != null) {
